95676dae4061db9c4ca9348870d562d8ad6b2e89
Anggie Alava <131820456+AngelesGit@users.noreply.github.com>
2023-08-17 00:05:56 +0000

    Cumpliedo

--- src/app.py
+++ src/app.py
@@ -6,9 +6,10 @@ from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
-from utils import APIException, generate_sitemap
+from utils import APIException, generate_sitemap 
 from admin import setup_admin
-from models import db, User
+from models import db, User , Character , Planet , Favorite
+from flask_sqlalchemy import SQLAlchemy
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +38,93 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def list_users():
+    user = User.query.all()
+    users_list = list(map(lambda x : x.to_dict(), user))
+    return jsonify(users_list)
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
 
-    return jsonify(response_body), 200
+@app.route('/people', methods=['GET'])
+def list_people():
+    people = Character.query.all()
+    people_list = list(map(lambda x : x.to_dict(), people))
+    return jsonify(people_list)
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = Character.query.get(people_id)
+    return jsonify(person.to_dict())
+
+
+@app.route('/planets', methods=['GET'])
+def list_planet():
+    planet = Planet.query.all()
+    planet_list = list(map(lambda x : x.to_dict(), planet))
+    return jsonify(planet_list)
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    return jsonify(planet.to_dict())
+
+@app.route('/favorites/character/<int:character_id>/<int:user_id>', methods=['POST'])
+def add_favorite_character(character_id, user_id):
+    favorite_character = Favorite(user_id = user_id, character_id = character_id)
+    db.session.add(favorite_character)
+    db.session.commit()
+    respuesta = {"mensaje" : "El personaje fue añadido a la lista de favoritos"}
+    return jsonify(respuesta)
+
+
+@app.route('/favorites/character/<int:character_id>/<int:user_id>', methods=['DELETE'])
+def delete_favorite_character(character_id, user_id):
+    
+    favorite_character = db.session.execute(
+            db.select(Favorite)
+            .filter(Favorite.user_id == user_id, Favorite.character_id == character_id)
+        ).scalar()
+    if favorite_character is None:
+        return jsonify({"mensaje" : "El personaje no esta en la lista de favoritos"}), 404
+    db.session.delete(favorite_character)
+    db.session.commit()
+    
+    respuesta = {"mensaje" : "El personaje fue ELIMINADO"}
+    print(favorite_character)
+    return jsonify(respuesta)
+
+
+@app.route('/favorites/planets/<int:planet_id>/<int:user_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id, user_id):
+    favorite_planet = db.session.execute(
+            db.select(Favorite)
+            .filter(Favorite.user_id == user_id, Favorite.planet_id == planet_id)
+        ).scalar()
+    if favorite_planet is None:
+        return jsonify({"mensaje" : "El planeta no esta en la lista de favoritos"}), 404
+    db.session.delete(favorite_planet)
+    db.session.commit()
+    
+    respuesta = {"mensaje" : "El planeta fue ELIMINADO"}
+    print(favorite_planet)
+    return jsonify(respuesta)
+
+
+@app.route('/favorites/planets/<int:planet_id>/<int:user_id>', methods=['POST'])
+def add_favorite_planet(planet_id, user_id):
+    favorite_planet = Favorite(user_id = user_id, planet_id = planet_id)
+    db.session.add(favorite_planet)
+    db.session.commit()
+    respuesta = {"mensaje" : "El planeta fue añadido a la lista de favoritos"}
+    return jsonify(respuesta)
+
+@app.route('/favorites', methods=['GET'])
+def get_all_favorites():
+    all_favorites= Favorite.query.all()
+    favorites_list = list(map(lambda x: x.to_dict(), all_favorites))
+    return jsonify(favorites_list), 200
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,48 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(120), unique=True, nullable=False)
+    first_name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)             
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+class Character(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    hair_color = db.Column(db.String(120), nullable=False)
+    eye_color = db.Column(db.String(120), nullable=False)
+    skin_color = db.Column(db.String(120), nullable=False)
+    gender = db.Column(db.String(120), nullable=False)
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+   
+
+class Planet(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    diameter = db.Column(db.Integer, nullable=False)
+    climate = db.Column(db.String(120), nullable=False)
+    terrain = db.Column(db.String(120), nullable=False)
+    population = db.Column(db.Integer, nullable=False)
+
+   
+
+class Favorite(db.Model, SerializerMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # user = db.relationship(User)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
+    # planet = db.relationship(Planet)
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
+    # character = db.relationship(Character)
+
+# Estoy probando un push
+
+    
